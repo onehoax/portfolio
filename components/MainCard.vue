3c5b27425ec5dbbcbf431a045fde67e2bee95be0
Andres Osorio <andres.osoriogr@gmail.com>
2023-10-03 11:39:51 -0500

    fix: responsive behavior in mobile

--- components/MainCard.vue
+++ components/MainCard.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink :to="page">
     <div
-      class="main-content h-44 flex flex-col justify-center text-center overflow-auto lg:w-2/3 lg:mx-auto"
+      class="main-content h-48 sm:h-44 flex flex-col justify-center text-center overflow-auto lg:w-2/3 lg:mx-auto"
       :class="{ 'mt-4': !isFirst }"
     >
       <h2 class="content-heading">{{ title }}</h2>
