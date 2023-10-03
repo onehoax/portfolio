54daabd45f65ab13cfcb8b6730b90fbf23b1db7d
Andres Osorio <andres.osoriogr@gmail.com>
2023-10-02 19:06:21 -0500

    add: resume page

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,6 +1,105 @@
 <template>
   <MainContent title="Resume">
-    <div class="main-content"></div>
+    <div class="main-content">
+      <h2 class="content-heading">Summary</h2>
+      <ul class="list-disc pl-5 mt-4">
+        <li>Full-Stack TypeScript Developer with experience in Java, Data Engineering, and the banking industry</li>
+        <li>
+          Led an internal background screening application at a Canadian Bank; successfully designed, implemented, and
+          delivered solutions to mitigate critical security risks
+        </li>
+        <li>3+ years of Software Development experience</li>
+        <li>12 years of total experience in teaching, behavior modification, and a variety of environments</li>
+        <li>Responsible, organized, disciplined, hard-working, fast learner, and goal-oriented</li>
+      </ul>
+      <h2 class="content-heading mt-6">Experience</h2>
+      <h3 class="content-subheading mt-4">Freelance Software Developer</h3>
+      <h4 class="font-medium">Self-Employed</h4>
+      <p>Jul 2022 - Present (1 year 3 months)</p>
+      <ul class="list-disc pl-5 mt-4">
+        <li>Applied agile methodologies to continuously revise product requirements based on client feedback</li>
+        <li>Successfully designed, implemented, and delivered excellent quality products on time and without bugs</li>
+        <li>
+          Developed Full-Stack solutions with TypeScript, NodeJS, ExpressJS, NestJS, VueJS, NuxtJS, Prisma ORM, and
+          PostgreSQL
+        </li>
+      </ul>
+      <h3 class="content-subheading mt-4">Full-Stack Developer</h3>
+      <h4 class="font-medium">CIBC</h4>
+      <p>Jan 2021 - Jul 2022 (1 year 7 months)</p>
+      <ul class="list-disc pl-5 mt-4">
+        <li>Lead Developer/SME for an internal background screening application</li>
+        <li>
+          Successfully designed, implemented, and delivered solutions to mitigate security risks identified in the
+          application on time and with no bugs; led a team of Senior Developers to accomplish this
+        </li>
+        <li>
+          Coordinated with internal and external teams to prioritize projects/tasks and deliver excellent quality
+          products on time
+        </li>
+        <li>
+          Participated in agile best practices through daily stand-up meetings, JIRA boards/tickets, and development
+          based on continuous requirement revision
+        </li>
+        <li>
+          Developed on top of a NICE Actimize product involving work with HTML/CSS, XSL, JavaScript, Java (Maven, Spring
+          Boot, Microservices, JBoss), Oracle DB, NiFi, BASH, Git, Hadoop, and Linux
+        </li>
+      </ul>
+      <h3 class="content-subheading mt-4">Data Engineer Associate</h3>
+      <h4 class="font-medium">Jarvis Consulting Group</h4>
+      <p>Jun 2020 - Jan 2021 (8 months)</p>
+      <ul class="list-disc pl-5 mt-4">
+        <li>
+          Developed Data Engineering projects through continuous participation in scrum events and agile best practices
+        </li>
+        <li>
+          Implemented projects using Java, Maven, Spring Boot, Bash, Python, RDBMS/SQL, Git, Hadoop, Spark/Scala,
+          Docker, and Kubernetes; performed both integration and unit testing for each project using JUnit and Mockito
+        </li>
+        <li>Used a Linux environment (CentOS) for development</li>
+      </ul>
+      <h3 class="content-subheading mt-4">Teaching & Behavior Modification</h3>
+      <h4 class="font-medium">Various</h4>
+      <p>Jan 2009 - Sep 2017 (8 years 9 months)</p>
+      <ul class="list-disc pl-5 mt-4">
+        <li>
+          Successfully improved lives of clients with autism and other developmental disorders through application of
+          behavior modification techniques
+        </li>
+        <li>Taught ESL to students of all ages and groups of all sizes</li>
+      </ul>
+      <h2 class="content-heading mt-6">Education</h2>
+      <h3 class="content-subheading mt-4">York University</h3>
+      <h4 class="font-medium">Bachelor of Arts - BA, Computer Science</h4>
+      <p>Sep 2017 - Dec 2019</p>
+      <h3 class="content-subheading mt-4">George Brown College</h3>
+      <h4 class="font-medium">Ontario College Advanced Diploma with Honours, Behavioral Science Technology</h4>
+      <p>Jan 2012 - Dec 2014</p>
+      <h2 class="content-heading mt-6">Skills</h2>
+      <h3 class="content-subheading mt-4">Concepts</h3>
+      <p>
+        OOP/FP, RDBMS/NoSQL, Design Patterns, Full-Stack Web Dev, Microservices, REST APIs, Containerization, Agile, TDD
+      </p>
+      <h3 class="content-subheading mt-4">Languages</h3>
+      <p>TypeScript, JavaScript, Java, SQL, Bash, Git, HTML/CSS</p>
+      <h3 class="content-subheading mt-4">Frameworks</h3>
+      <p>
+        NodeJS, ExpressJS, NestJS, VueJS, NuxtJS, Prisma ORM, PostgreSQL, OracleDB, GitHub, Docker, Maven, SpringBoot
+      </p>
+      <h3 class="content-subheading mt-4">Other</h3>
+      <p>Cloud, Haskell, Blockchain</p>
+      <h2 class="content-heading mt-6">Honors & Awards</h2>
+      <h3 class="content-subheading mt-4">Chair's Honour Roll</h3>
+      <h4 class="font-medium">York University</h4>
+      <p>
+        Chair's Honour Roll for the school year of 2017-2018; remained in the top 15% of my faculty (Electrical
+        Engineering and Computer Science) throughout all the terms of the program.
+      </p>
+      <h3 class="content-subheading mt-4">Dean's Honour List</h3>
+      <h4 class="font-medium">George Brown College</h4>
+      <p>Term Grade Point Average of 3.5 or higher throughout all terms of the program.</p>
+    </div>
   </MainContent>
 </template>
 
